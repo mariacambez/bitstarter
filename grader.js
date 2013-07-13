d369a65844e3f6d6fa616418918013c991ab237d
Maria Cambez <mariacambez@gmail.com>
2013-07-13 01:57:45 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/*
+/*1!
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
@@ -21,6 +21,7 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var rest=require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
@@ -34,6 +35,17 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var scrapeURL= function(url) {
+    rest.get(url).on('complete',function(result){
+	if(result instanceof Error) {
+	    console.log("error,exit!")
+	    process.exit(1);
+	}else {
+	fs.writeFileSync(HTMLFILE_DEFAULT,result);
+	}
+	});    
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +77,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'Path to index.html', clone(scrapeURL),HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
